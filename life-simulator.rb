195b785238c37852d4102abcc210fb9cd881eeec
ya-kyrylenko <ya.kyrylenko@gmail.com>
2015-04-02 17:34:37 +0300

    Another gommit

--- life-simulator.rb
+++ life-simulator.rb
@@ -1,8 +1,15 @@
 class Universe
 	attr_accessor :rows, :columns, :grid
-	def initialize(rows=5, columns=5)
+	def initialize(rows=3, columns=3)
 		@rows = rows
 		@columns = columns
-		@grid = Array.new(rows) { Array.new(columns) {} }
+		@grid = Array.new(rows) { Array.new(columns) { Cell.new} }
+	end
+end
+
+class Cell
+	attr_accessor :alive
+	def initialize
+		@alive = false
 	end
 end
--- life_simulator_spec.rb
+++ life_simulator_spec.rb
@@ -2,12 +2,28 @@ require 'rspec'
 require_relative 'life-simulator.rb'
 
 describe "Game" do
-	describe "Universe" do
+	describe "Universe class" do
 		subject { Universe.new }
-		# Should create a new oject
-		it { expect(subject.is_a?(Universe)).to be_truthy }
-		it { expect(subject.grid.is_a?(Array)).to be_truthy }
+		# Should create a new Universe oject
+#		it { expect(subject.is_a?(Universe)).to be_truthy }
+		it { expect(subject).to be_a Universe }
+#		it { expect(subject.grid.is_a?(Array)).to be_truthy }
+		it { expect(subject.grid).to be_a Array }
 		it { should respond_to(:rows, :columns, :grid) }
+		it 'Array must create Cell class' do
+			subject.grid.each do |row|
+				row.each do |colum|
+					expect(colum).to be_a Cell
+				end
+			end
+		end
+	end
 
+	describe "Cell class" do
+		subject{ Cell.new }
+		# Should create a new Cell oject
+		it { expect(subject).to be_a Cell }
+		it { should respond_to(:alive) }
+		it { expect(subject.alive).to eq false}
 	end
 end
