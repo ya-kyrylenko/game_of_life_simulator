596cb5c5554c5af2d70c8bfd3e9290e999c0ca7d
ya-kyrylenko <ya.kyrylenko@gmail.com>
2015-04-02 15:52:56 +0300

    Add a grid and rspec

--- life-simulator.rb
+++ life-simulator.rb
@@ -1,2 +1,8 @@
 class Universe
+	attr_accessor :rows, :columns, :grid
+	def initialize(rows=5, columns=5)
+		@rows = rows
+		@columns = columns
+		@grid = Array.new(rows) { Array.new(columns) {} }
+	end
 end
--- life_simulator_spec.rb
+++ life_simulator_spec.rb
@@ -4,8 +4,10 @@ require_relative 'life-simulator.rb'
 describe "Game" do
 	describe "Universe" do
 		subject { Universe.new }
-		it 'should create a new oject' do
-			expect(subject.is_a?(Universe)).to be_truthy
-		end
+		# Should create a new oject
+		it { expect(subject.is_a?(Universe)).to be_truthy }
+		it { expect(subject.grid.is_a?(Array)).to be_truthy }
+		it { should respond_to(:rows, :columns, :grid) }
+
 	end
 end
