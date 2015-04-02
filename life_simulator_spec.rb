0c935f3f1218a3163921f267f9d0b561920f8337
ya-kyrylenko <ya.kyrylenko@gmail.com>
2015-04-02 14:47:59 +0300

    start develop

--- life_simulator_spec.rb
+++ life_simulator_spec.rb
@@ -1,1 +1,11 @@
 require 'rspec'
+require_relative 'life-simulator.rb'
+
+describe "Game" do
+	describe "Universe" do
+		subject { Universe.new }
+		it 'should create a new oject' do
+			expect(subject.is_a?(Universe)).to be_truthy
+		end
+	end
+end
