a754f35b6050c4eb437996358ae791bad40f5938
ya-kyrylenko <ya.kyrylenko@gmail.com>
2015-04-03 08:12:25 +0300

    add Rules and try test it

--- life-simulator.rb
+++ life-simulator.rb
@@ -1,15 +1,127 @@
 class Universe
-	attr_accessor :rows, :columns, :grid
-	def initialize(rows=3, columns=3)
+	attr_accessor :rows, :columns, :grid, :cells
+	def initialize(rows = 3, columns = 3)
 		@rows = rows
 		@columns = columns
-		@grid = Array.new(rows) { Array.new(columns) { Cell.new} }
+		@cells = []
+		@grid = Array.new(rows) { |row| Array.new(columns) do |column|
+			cell = Cell.new(column, row)
+			cells << cell
+			cell
+		 	end}
+	end
+
+	def neighbours(cell)
+		live_neighbours = []
+		# Detects a neighbour on top
+		if cell.y > 0
+			neighbour = self.grid[cell.y - 1][cell.x]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		# Detects a neighbour on top-left
+		if cell.y > 0 and cell.x > 0
+			neighbour = self.grid[cell.y - 1][cell.x - 1]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		# Detects a neighbour on top-right
+		if cell.y > 0 and cell.x < (columns - 1)
+			neighbour = self.grid[cell.y - 1][cell.x + 1]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		# Detects a neighbour on left
+		if cell.x > 0
+			neighbour = self.grid[cell.y][cell.x - 1]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		# Detects a neighbour on right
+		if cell.x < (columns - 1)
+			neighbour = self.grid[cell.y][cell.x + 1]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		# Detects a neighbour on bottom
+		if cell.y < (rows - 1)
+			neighbour = self.grid[cell.y + 1][cell.x]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		# Detects a neighbour on bottom-left
+		if cell.y < (rows - 1) and cell.x > 0
+			neighbour = self.grid[cell.y + 1][cell.x - 1]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		# Detects a neighbour on bottom-right
+		if cell.y < (rows - 1) and cell.x < (columns - 1)
+			neighbour = self.grid[cell.y + 1][cell.x + 1]
+			live_neighbours << neighbour if neighbour.alive?
+		end
+		live_neighbours
 	end
 end
 
+
 class Cell
-	attr_accessor :alive
-	def initialize
+	attr_accessor :alive, :x, :y
+	def initialize(x = 0, y = 0)
 		@alive = false
+		@x = x
+		@y = y
+	end
+
+	def alive?
+		alive
+	end
+
+	def not_alive?
+		!alive
+	end
+
+	def die!
+		@alive = false
+	end
+
+	def come_to_alive!
+		@alive = true
+	end
+end
+
+
+class LogicGame
+	attr_accessor :universe, :fortestcells
+	def initialize(universe = Universe.new, fortestcells = [])
+		@universe = universe
+		@fortestcells = fortestcells
+
+		fortestcells.each do |testcell|
+			# testseel[0] - y, testcell[1] - x.
+			universe.grid[testcell[0]][testcell[1]].alive = true
+		end
+	end
+
+	def step!
+		will_live_cells = []
+		will_die_cells = []
+		universe.cells.each do |cell|
+			# Rules 1
+			if cell.alive? && universe.neighbours(cell).count < 2
+				will_die_cells << cell
+			end
+			# Rules 2
+			if cell.alive? && universe.neighbours(cell).count > 3
+				will_die_cells << cell
+			end
+			# Rules 3
+			if cell.alive? && ([2, 3].include? universe.neighbours(cell).count)
+				will_live_cells << cell
+			end
+			# Rules 4
+			if cell.not_alive? && universe.neighbours(cell).count == 3
+				will_live_cells << cell
+			end
+		end
+		will_live_cells.each { |cell|
+			cell.come_to_alive!
+		}
+		will_die_cells.each { |cell|
+			cell.die!
+		}
 	end
 end
--- life_simulator_spec.rb
+++ life_simulator_spec.rb
@@ -2,14 +2,14 @@ require 'rspec'
 require_relative 'life-simulator.rb'
 
 describe "Game" do
+	let!(:universe) { Universe.new }
+	let!(:cell) { Cell.new(1, 1) }
 	describe "Universe class" do
 		subject { Universe.new }
 		# Should create a new Universe oject
-#		it { expect(subject.is_a?(Universe)).to be_truthy }
 		it { expect(subject).to be_a Universe }
-#		it { expect(subject.grid.is_a?(Array)).to be_truthy }
 		it { expect(subject.grid).to be_a Array }
-		it { should respond_to(:rows, :columns, :grid) }
+		it { should respond_to(:rows, :columns, :grid, :cells) }
 		it 'Array must create Cell class' do
 			subject.grid.each do |row|
 				row.each do |colum|
@@ -17,13 +17,87 @@ describe "Game" do
 				end
 			end
 		end
+		it 'Detects live neighbour to the top' do
+			subject.grid[cell.y - 1][cell.x].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
+		it 'Detects live neighbour to the top-right' do
+			subject.grid[cell.y - 1][cell.x + 1].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
+		it 'Detects live neighbour to the top-left' do
+			subject.grid[cell.y - 1][cell.x - 1].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
+		it 'Detects live neighbour to the left' do
+			subject.grid[cell.y][cell.x - 1].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
+		it 'Detects live neighbour to the right' do
+			subject.grid[cell.y][cell.x + 1].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
+		it 'Detects live neighbour to the bottom' do
+			subject.grid[cell.y + 1][cell.x].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
+		it 'Detects live neighbour to the bottom-left' do
+			subject.grid[cell.y + 1][cell.x - 1].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
+		it 'Detects live neighbour to the bottom-right' do
+			subject.grid[cell.y + 1][cell.x + 1].alive = true
+			expect(subject.neighbours(cell).count).to eq 1
+		end
 	end
 
 	describe "Cell class" do
 		subject{ Cell.new }
 		# Should create a new Cell oject
 		it { expect(subject).to be_a Cell }
-		it { should respond_to(:alive) }
+		it { should respond_to(:alive, :x, :y) }
 		it { expect(subject.alive).to eq false}
+	end
+	describe "Logic game" do
+		subject { LogicGame.new }
+		it { expect(subject).to be_a LogicGame }
+		it { should respond_to(:universe)}
+		it 'testing method for rules' do
+			LogicGame.new(universe, [[0,2], [1,2]])
+			expect(universe.grid[0][2].alive).to eq true
+			expect(universe.grid[1][2].alive).to eq true
+		end
+	end
+	describe "Rules of game" do
+		let!(:game) { LogicGame.new }
+		it 'kill a live cell with 1 neighbour' do
+			game = LogicGame.new(universe, [[0, 1], [1, 1]])
+			#expect(game.universe.grid[1][1].alive).to eq true
+			game.step!
+			game.universe.grid[1][1].should be_not_alive
+			# expect(universe.grid[1][1].alive).to eq false
+			expect(universe.grid[0][1].alive).to eq false
+		end
+		it 'Should keep alive cell with 2 neighbours' do
+			game = LogicGame.new(universe, [[0, 1], [1, 1], [2, 1]])
+			universe.neighbours(universe.grid[1][1]).count.should == 2
+			game.step!
+			universe.grid[0][1].should be_not_alive
+			universe.grid[1][1].should be_alive
+			universe.grid[2][1].should be_not_alive
+		end
+		it 'Should die cell with 4 neighbours' do
+			game = LogicGame.new(universe, [[0, 1], [1, 1], [2, 1], [1, 0], [0, 2]])
+			universe.neighbours(universe.grid[1][1]).count.should == 4
+			game.step!
+			universe.grid[1][1].should be_not_alive
+		end
+		it 'Should come alive cell with 3 neighbours' do
+			game = LogicGame.new(universe, [[0, 1], [1, 1], [2, 1], [1, 0]])
+			universe.grid[1][1].alive = false
+			universe.neighbours(universe.grid[1][1]).count.should == 3
+			game.step!
+			universe.grid[1][1].should be_alive
+		end
 	end
 end
